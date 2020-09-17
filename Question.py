78e294e90b41dbb711b24e60cead33840c3e32e5
michaelzhang0608 <61032096+michaelzhang0608@users.noreply.github.com>
2020-09-17 11:42:17 -0400

    Update Question.py

--- Question.py
+++ Question.py
@@ -4,12 +4,12 @@ import random
 
 
 class Question():
-    def __init__(self, question_text, question_id, question_audio):
+    def __init__(self, question_text, question_id, question_audio, explanation_text, explanation_audio):
         self.question_text = question_text
         self.question_id = question_id
         self.question_audio = question_audio
-        self.explanation_text = None
-        self.explanation_audio = None
+        self.explanation_text = explanation_text
+        self.explanation_audio = explanation_audio
 
     #plays question audio
     def play_question_audio(self):
