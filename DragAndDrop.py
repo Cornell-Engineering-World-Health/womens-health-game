2453034118e2d264e638332d455d8816c0d40f61
michaelzhang0608 <61032096+michaelzhang0608@users.noreply.github.com>
2020-09-17 11:43:18 -0400

    Update DragAndDrop.py

--- DragAndDrop.py
+++ DragAndDrop.py
@@ -8,10 +8,7 @@ class DragAndDrop(Question):
 
     def __init__(self, question_id, question_text, question_audio, explanation_text, explanation_audio,
                  ordered_image_ids, current_answer):
-        Question.__init__(self, question_id, question_text, question_audio)
-        self.explanation_text = explanation_text
-        self.explanation_audio = explanation_audio
-        self.default_current_answer = []
+        Question.__init__(self, question_id, question_text, question_audio, explanation_text, explanation_audio)
         self.ordered_image_ids = ordered_image_ids
         self.current_answer = current_answer
 
