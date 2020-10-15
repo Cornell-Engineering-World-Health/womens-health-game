4ff2fd79fbd8d1b40044dc77999a1ece5fcdb40d
Shaf Ibrahim <smi32@cornell.edu>
2020-10-14 19:06:10 -0700

    User flow with script working

--- ui/module.py
+++ ui/module.py
@@ -18,13 +18,25 @@ class Module(Screen):
         self.app = MDApp.get_running_app()
 
         self.user = None
+        # Current module
         self.module_number = 0
+        # List of scenes in the current module
         self.scenes = []
+        # All the character objects in the scene
         self.scene_characters = []
+        # Array of available positions on the screen (3 total)
         self.screen_positions = []
+        # Number of characters currently on the screen
         self.screen_characters = 0
+        # Current scene object
         self.current_scene = None
+        # The current line in the script that is going to be played
         self.script_iterator = -1
+        # List of lines (not actions) that have been executed
+        self.lines = []
+        # The index of the line that was executed; default is -1
+        self.line_iterator = -1
+        # Current dialogue label (to be replaced with audio file)
         self.current_line = MDLabel(
             pos_hint= {"x": .3, "top": 1},
             size_hint= (.4, .1),
@@ -59,25 +71,9 @@ class Module(Screen):
     # Screen positioning: assuming maximum of 3 characters on screen at one time
     def _init_module_ui(self):
         # 3 character positions on screen at once
-        self.screen_positions.append({'x': -0.3, 'top': 0.75})
-        self.screen_positions.append({'x': 0, 'top': 0.9})
-        self.screen_positions.append({'x': 0.3, 'top': 0.75})
-
-        # Add next and previous buttons
-        # Change icon directory to assets/button...
-        # Ripple effect on click?
-        # next_button = MDIconButton(
-        #     icon="assets/character-mouths/idle_happy.png",
-        #     pos_hint={'x': 0.9, 'top': 0.5},
-        #     user_font_size='64sp'
-        # )
-        # prev_button = MDIconButton(
-        #     icon="assets/character-mouths/idle_happy.png",
-        #     pos_hint={'x': 0, 'top': 0.5},
-        #     user_font_size='64sp'
-        # )
-        # self.ids.float.add_widget(next_button)
-        # self.ids.float.add_widget(prev_button)
+        self.screen_positions.append({'x': -0.3, 'top': 0.45})
+        self.screen_positions.append({'x': 0, 'top': 0.6})
+        self.screen_positions.append({'x': 0.3, 'top': 0.45})
 
     def _load_module(self, module_number):
         self._load_module_json(module_number)
@@ -112,48 +108,6 @@ class Module(Screen):
             self.scenes.append(scene)
             self.current_scene = self.scenes[0]
 
-    def advance_line(self):
-        if (self.script_iterator < len(self.current_scene.script)):
-            self.script_iterator += 1
-            line = self.current_scene.script[self.script_iterator]
-            print(line)
-            self.play_current_line(line)
-        
-    # When rewinding a line, should we remove characters that were just added?
-    # Need to handle adding the same character twice when rewinding
-    def previous_line(self):
-        if (self.script_iterator > 0):
-            self.script_iterator -= 1
-            line = self.current_scene.script[self.script_iterator]
-            print(line)
-            self.play_current_line(line)
-
-    # def render_scene(self):
-    #     scene = self.current_scene
-    #     script = scene.script
-    #     self._load_characters()
-    #     print(script)
-
-    #     for script_line in script:
-    #         print(script_line)
-    #         self.play_current_line(script_line)
-    #     self.scene_characters = []
-
-    def play_current_line(self, line):
-        if (type(line) == Line):
-            self.current_line.text = line.text
-        else:
-            line_character = None
-            for character in self.scene_characters:
-                if character.id == line.character_id:
-                    line_character = character
-            if (line.action_type == 'enter'):
-                self.screen_characters += 1
-                self._render_character(line_character)
-            else:
-                self.screen_characters -= 1
-                self._remove_character(line_character)
-
     def _load_characters(self):
         json_file_path = 'assets/json/characters.json'
 
@@ -169,10 +123,82 @@ class Module(Screen):
                     character_obj = Character(char_id, char_name, char_image)
                     self.scene_characters.append(character_obj)
 
+    # Plays the current line and advances the script_iterator
+    def advance_line(self):
+        self.script_iterator += 1
+        if (self.script_iterator < len(self.current_scene.script)):
+            # Check if a line has been executed already
+            line = self.current_scene.script[self.script_iterator]
+            if (type(line) == Line):
+                self.lines.append(line)
+                self.line_iterator += 1
+            print('Executing: ' + str(line))
+            self.play_line(line)
+        else:
+            # Set script iterator to end of script
+            self.script_iterator = len(self.current_scene.script) - 1
+        
+    # Rewinds the line that was just played and plays the prev line
+    def previous_line(self):
+        if (self.script_iterator >= 0):
+            # Undo line that was just played
+            line = self.current_scene.script[self.script_iterator]
+            print('Rewinding: ' + str(line))
+            # If an Action was just played:
+            # Remove character if it just entered, or add character if it was just removed
+            if (type(line) == Action):
+                if (line.action_type == 'enter'):
+                    line_character = None
+                    for character in self.scene_characters:
+                        if character.id == line.character_id:
+                            line_character = character
+                    self._remove_character(line_character)
+                else:
+                    self._render_character(line_character)
+            # If current line was a Line:
+            # Play previous line
+            else:
+                # Check if a line in the script has been executed yet
+                if (self.line_iterator > 0):
+                    self.line_iterator -= 1
+                    self.lines.pop()
+                    self.play_line(self.lines[self.line_iterator])
+                else:
+                    # Remove dialogue label if no line has been executed
+                    self.current_line.text = ""
+                    # Update tracking variables
+                    self.lines = []
+                    self.line_iterator = -1
+            self.script_iterator -= 1
+
+    # def render_scene(self):
+    #     scene = self.current_scene
+    #     script = scene.script
+    #     self._load_characters()
+    #     print(script)
+
+    #     for script_line in script:
+    #         print(script_line)
+    #         self.play_current_line(script_line)
+    #     self.scene_characters = []
+
+    def play_line(self, line):
+        if (type(line) == Line):
+            self.current_line.text = line.text
+        else:
+            line_character = None
+            for character in self.scene_characters:
+                if character.id == line.character_id:
+                    line_character = character
+            if (line.action_type == 'enter'):
+                self._render_character(line_character)
+            else:
+                self._remove_character(line_character)
+
     def _render_character(self, character):
         # Load image from character as kivy object
         image_file_path = 'assets/characters/' + character.image
-        # Position character based off # of characters on screen
+        # Position character based off current # of characters on screen
         pos = self._position_character()
         new_character = Image(
             source=image_file_path,
@@ -182,13 +208,23 @@ class Module(Screen):
             id=str(character.id)
         )
         self.ids.float.add_widget(new_character)
+        self.screen_characters += 1
 
     # Returns a Kivy position as a dictionary (x and top)
     def _position_character(self):
-        return self.screen_positions[self.screen_characters - 1]
+        return self.screen_positions[self.screen_characters]
 
     def _remove_character(self, character):
-        self.ids.float.remove_widget(character)
+        for widget in self.ids.float.children:
+            # Check if widget has associated ID (not a button)
+            if (widget.id):
+                try:
+                    # self.ids.float.remove_widget(widget) if (int(widget.id) == character.id)
+                    if (int(widget.id) == character.id):
+                        self.ids.float.remove_widget(widget)
+                        self.screen_characters -= 1
+                except:
+                    pass
 
     def load_assessment(self):
         if self.user:
