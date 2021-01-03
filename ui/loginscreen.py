a5e232c34319401a64af90a02e27ee9cea71682d
tklausner <klausnerteddy@gmail.com>
2021-01-02 19:35:30 -0700

    added label for error messages in login screen

--- ui/loginscreen.py
+++ ui/loginscreen.py
@@ -1,10 +1,11 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
-
+from kivy.properties import StringProperty
 from util.client import login
 
 
 class LoginScreen(Screen):
+    error_message = StringProperty("")
     def __init__(self, **kw):
         super().__init__(**kw)
         self.app = MDApp.get_running_app()
@@ -13,7 +14,7 @@ class LoginScreen(Screen):
         self.app.title = "Login"
 
     """
-    Called when the user presses the login button. 
+    Called when the user presses the login button.
     """
     def login(self):
         status = login(self.login_email, self.login_password)
@@ -27,17 +28,24 @@ class LoginScreen(Screen):
             self.manager.current = 'dashboard'
 
         #login unsuccesful
-        elif status[1] == "login_failure": 
+        elif status[1] == "login_failure":
             print("invalid username | password")
-            #TODO: add "Invalid username or password" to the screen
-            pass
+            self.process_error("Invalid username or password")
+
         elif status[1] == "network_failure":
             print("network failure")
-            #TODO: add message "Something went wrong on our end, please try again in a couple minutes. If this message persists, please contact Cornell Engineering World Health for support."
-            pass
+            self.process_error("Something went wrong on our end, please try again in a couple minutes. If this message persists, please contact Cornell Engineering World Health for support.")
 
     def process_email(self):
+        self.clear_error()
         self.login_email = self.ids.email.text
 
     def process_password(self):
+        self.clear_error()
         self.login_password = self.ids.password.text
+
+    def clear_error(self):
+        self.error_message = ""
+
+    def process_error(self, message):
+        self.error_message = message
